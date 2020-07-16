d9812e72bc6f6516417f4e4fe8feb86631603690
Marcelo Henrique Cezário <33532147+marcelocezario@users.noreply.github.com>
2020-07-16 17:18:56 -0300

    Autenticando com token

---
+++ src/middlewares/auth.js
@@ -0,0 +1,30 @@
+const jwt = require('jsonwebtoken');
+const authConfig = require('../config/auth.json');
+
+module.exports = (req, res, next) => {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader)
+        return res.status(401).send({ error: 'No token provided' });
+
+    // Bearer + hash
+    const parts = authHeader.split(' ');
+
+    if (!parts.length === 2)
+        return res.status(401).send({ error: 'Token error' });
+
+    const [ scheme, token ] = parts;
+
+    // Regex
+    if (!/^Bearer$/i.test(scheme))
+        return res.status(401).send({ error: 'Token malformatted' });
+
+    jwt.verify(token, authConfig.secret, (err, decoded) => {
+        if (err) return res.status(401).send({ error: 'Token invalid' });
+
+        req.userId = decoded.id;
+        return next();
+    });
+        
+    
+};
