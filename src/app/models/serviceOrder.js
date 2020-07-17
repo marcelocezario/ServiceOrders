f572e0ad36af6abbaeec870999c5b0223e899640
Marcelo Henrique Cezário <33532147+marcelocezario@users.noreply.github.com>
2020-07-17 00:52:53 -0300

    Criando model serviceOrder.js

---
+++ src/app/models/serviceOrder.js
@@ -0,0 +1,39 @@
+const mongoose = require('../../database');
+
+const ServiceOrderSchema = new mongoose.Schema({
+    number: {
+        type: Number,
+        require: true,
+    },
+    client: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Client',
+        require: true,
+    },
+    status: {
+        type: String,
+        enum: ['open', 'closed', 'canceled', 'paid'],
+        require: true,
+        default: 'open'
+    },
+    services: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Service'
+    }],
+    amount: {
+        type: number,
+        require: true,
+    },
+    payday: {
+        type: date,
+        require: false,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+const Client = mongoose.model('Client', UserSchema);
+
+module.exports = Client;
