a7ca65ad52370949396b8d1e4f07f7fd910973b6
Marcelo Henrique Cezário <33532147+marcelocezario@users.noreply.github.com>
2020-07-17 01:14:45 -0300

    Update projectController.js

--- src/app/controllers/projectController.js
+++ src/app/controllers/projectController.js
@@ -1,12 +1,32 @@
 const express = require('express');
 const authMiddleware = require('../middlewares/auth');
 
+const Project = require('../models/Project');
+const Task = require('../models/Task');
+
 const router = express.Router();
 
 router.use(authMiddleware);
 
 router.get('/', (req, res) => {
     res.send({ ok: true, user: req.userId });
+});
+
+
+router.get('/:projectId', async (req, res) => {
+    res.send({ user: req.userId });
+});
+
+router.post('/', async (req, res) => {
+    res.send({ ok: true, user: req.userId });
+});
+
+router.put('/:projectId', async (req, res) => {
+    res.send({ user: req.userId });
+});
+
+router.delete('/:projectId', async (req, res) => {
+    res.send({ user: req.userId });
 });
 
 module.exports = app => app.use('/projects', router);
