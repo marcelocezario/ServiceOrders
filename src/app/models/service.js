bb4f93e396c4eb4cad65e703a1ec60cb1c49dc1b
Marcelo Henrique Cezário <33532147+marcelocezario@users.noreply.github.com>
2020-07-17 01:01:34 -0300

    Criando model service.js

---
+++ src/app/models/service.js
@@ -0,0 +1,33 @@
+const mongoose = require('../../database');
+
+const ServiceSchema = new mongoose.Schema({
+    description: {
+        type: String,
+        require: true,
+    },
+    serviceOrder: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'ServiceOrder',
+        require: true,
+    },
+    value: {
+        type: Number,
+        require: true,
+    },
+    discount: {
+        type: Number,
+        require: true,
+    },
+    finalValue: {
+        type: Number,
+        require: true,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+const Service = mongoose.model('Service', UserSchema);
+
+module.exports = Service;
