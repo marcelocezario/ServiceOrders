c7bc1731198781b337466f8317c104fc6d9286e2
Marcelo Henrique Cezário <33532147+marcelocezario@users.noreply.github.com>
2020-07-17 01:20:57 -0300

    Update client.js

--- src/app/models/client.js
+++ src/app/models/client.js
@@ -23,6 +23,6 @@ const ClientSchema = new mongoose.Schema({
     },
 });
 
-const Client = mongoose.model('Client', UserSchema);
+const Client = mongoose.model('Client', ClientSchema);
 
 module.exports = Client;
