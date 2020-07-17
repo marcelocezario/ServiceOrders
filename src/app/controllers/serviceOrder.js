44a7bf5555b8e9cd09b0bbda3811dce8ca8c8f08
Marcelo Henrique Cezário <33532147+marcelocezario@users.noreply.github.com>
2020-07-17 15:24:28 -0300

    Incluindo alterar serviceOrder validando status

--- src/app/controllers/serviceOrder.js
+++ src/app/controllers/serviceOrder.js
@@ -4,6 +4,7 @@ const authMiddleware = require ('../middlewares/auth');
 const ServiceOrder = require('../models/serviceorder');
 const Service = require('../models/service');
 const Client = require('../models/client');
+const { stat } = require('fs');
 
 const router = express.Router();
 
@@ -72,26 +73,59 @@ router.post('/', async (req, res) => {
         return res.status(400).send({ error: 'Error creating new service order' + err});
     }
 });
-/*
-router.put('/:clientId', async (req, res) => {
+
+router.put('/:serviceOrderId', async (req, res) => {
     try {
-        const { name, adress, phone, email } = req.body;
+        const { client, services, status } = req.body;
 
-        const client = await Client.findByIdAndUpdate(req.params.clientId, {
-            name,
-            adress,
-            phone,
-            email,
-        }, { new: true });
+        const oldOrder = await ServiceOrder.findById(req.params.serviceOrderId);
 
-        await client.save();
+        if ( oldOrder.status !== "open" )
+            return res.status(400).send({ error: 'Its not allowed to edit an order with ' + oldOrder.status +' status'});
 
-        return res.send({ client });
+        const serviceOrder = await ServiceOrder.findByIdAndUpdate(req.params.serviceOrderId, { status });
+
+        serviceOrder.client = [];
+        serviceOrder.services = [];
+        await Service.deleteMany({ serviceOrder: serviceOrder._id });
+
+        await Promise.all(client.map(async client => {
+            const serviceOrderClient = new Client({ ...client });
+
+            await serviceOrderClient.save();
+
+            serviceOrder.client = serviceOrderClient;
+        }));
+
+
+        await Promise.all(services.map(async service => {
+            
+            const serviceOrderService = new Service({
+                ...service,
+                finalValue: service.value * (1- service.discount),
+                serviceOrder: serviceOrder._id
+            });
+
+            await serviceOrderService.save();
+
+            serviceOrder.services.push(serviceOrderService);
+
+        }));
+
+        serviceOrder.amount = 0
+
+        serviceOrder.services.forEach(service => {
+            serviceOrder.amount += service.finalValue      
+        });
+
+        await serviceOrder.save();
+ 
+        return res.send({ serviceOrder });
     } catch (err) {
-        return res.status(400).send({ error: 'Error editing client'});
+        return res.status(400).send({ error: 'Error update service order' + err});
     }
 });
-
+/*
 router.delete('/:clientId', async (req, res) => {
     try {
         await Client.findByIdAndRemove(req.params.clientId);
